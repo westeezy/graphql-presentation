d9d87303aada666e514f91bb8ecb0d71ee912806
Westin Wrzesinski <westinw@slalom.com>
2015-09-27 11:17:25 -0500

    Refactor

---
+++ README.md
@@ -0,0 +1,15 @@
+Built with Node 4.0.0, but should work on anything above 0.12 with
+harmony.
+
+If using nvm run `nvm install 4.0` followed by `nvm use 4.0`
+
+Then typical node setup task of `npm install` to get deps
+
+To seed DB run `npm run seed`
+To test run `npm run test`
+To show CLI example run `npm run client` (modifies DB so reseed after if
+doing demos).
+To run browser demo run `npm start` and navigate to `localhost:8000`
+
+To view slides in the slides directory run
+`npm install` followed by `grunt` and navigate to `localhost:9000`.
---
+++ src/server/server.js
@@ -0,0 +1,69 @@
+import koa from 'koa';
+import Router from 'koa-router';
+import qs from 'koa-qs';
+import views from 'co-views';
+import parseBody from 'co-body';
+import mongoose from 'mongoose';
+import {graphql} from 'graphql';
+import Debug from 'debug';
+import schema from './schema';
+
+let debug = new Debug('http');
+let render = views(__dirname + '/views', {
+  map: { html: 'swig' }
+});
+
+let port = process.env.PORT || 3000;
+let routes = new Router();
+var app = koa();
+
+// support nested query tring params
+qs(app);
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/graphql');
+}
+
+routes.get('/', function* () {
+  this.body = yield render('index');
+});
+
+routes.get('/data', function* () {
+  var query = this.query.query;
+  var params = this.query.params;
+
+  var resp = yield graphql(schema, query, '', params);
+
+  if (resp.errors) {
+    this.status = 400;
+    this.body = {
+      errors: resp.errors
+    };
+    return;
+  }
+
+  this.body = resp;
+});
+
+routes.post('/data', function* () {
+  var payload = yield parseBody(this);
+  var resp = yield graphql(schema, payload.query, '', payload.params);
+
+  if (resp.errors) {
+    this.status = 400;
+    this.body = {
+      errors: resp.errors
+    };
+    return;
+  }
+
+  this.body = resp;
+});
+
+app.use(routes.middleware());
+
+app.listen(port, () => {
+  debug('app is listening on %s', port);
+});
+
+module.exports = app;
